f0ae60c00d580b0a7f8824a9e53178eed9c3cc19
julioborn <juliobornes10@gmail.com>
2025-04-28 18:35:06 -0300

    PWA

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+import withPWA from "next-pwa";
+
+const isDev = process.env.NODE_ENV === "development";
+
+const nextConfig = withPWA({
+    dest: "public",
+    disable: isDev
+});
 
 export default nextConfig;
